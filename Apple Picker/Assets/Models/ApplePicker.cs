3e97c67efa36a2ac20014a14c01e3adc7b0ef8f9
SimranRiar <simriar@gmail.com>
2018-06-12 16:56:56 -0400

    Issue: Apple Destruction Not Registering
    Apples being dropped aren't registered.
    Therefore,
    Baskets aren't being destroyed.
    Therefore,
    The game can't end.

--- Apple Picker/Assets/Models/ApplePicker.cs
+++ Apple Picker/Assets/Models/ApplePicker.cs
@@ -1,26 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class ApplePicker : MonoBehaviour {
+public class ApplePicker : MonoBehaviour
+{
 
     public GameObject basketPrefab;
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
-    void Start () {
-        for (int i=0; i<numBaskets; i++)
+    void Start()
+    {
+        basketList = new List<GameObject>();
+
+        for (int i = 0; i < numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    public void AppleDestroyed()
+    {
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+
+            int basketIndex = basketList.Count - 1;
+
+            GameObject tBasketGo = basketList[basketIndex];
+
+            basketList.RemoveAt(basketIndex);
+
+            Destroy(tBasketGo);
+
+            if (basketList.Count == 0)
+            {
+                SceneManager.LoadScene("_Scene_0");
+            }
+        }
 }
