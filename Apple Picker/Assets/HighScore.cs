21427e88ec345a3c29a45b7e7d147fd603655a03
SimranRiar <simriar@gmail.com>
2018-06-12 20:24:13 -0400

    9 - Carry Over HighScore Value When Restarting

--- Apple Picker/Assets/HighScore.cs
+++ Apple Picker/Assets/HighScore.cs
@@ -5,10 +5,25 @@ using UnityEngine.UI;
 
 public class HighScore : MonoBehaviour {
 
-    static public int score = 1000;
+    static public int score = 0;
+
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+
+        PlayerPrefs.SetInt("HighScore", score);
+    }
 
 	void Update () {
         Text gt = this.GetComponent<Text>();
         gt.text = "High Score: " + score;
+
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
 	}
 }
